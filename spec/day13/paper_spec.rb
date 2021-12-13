0d066ce9e83147578dd8fd2566e220ea5ffb8863
Tudor Pavel <tudorv.pavel@gmail.com>
2021-12-13 20:45:38 +0200

    Avoid global constants

--- spec/day13/paper_spec.rb
+++ spec/day13/paper_spec.rb
@@ -3,32 +3,32 @@
 
 require_relative '../../day13/paper'
 
-INPUT = %(
-6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5
-)
-
 RSpec.describe Day13::Paper do
-  subject(:paper) { described_class.build(INPUT) }
+  subject(:paper) do
+    described_class.build(%(
+      6,10
+      0,14
+      9,10
+      0,3
+      10,4
+      4,11
+      6,0
+      6,12
+      4,1
+      0,13
+      10,12
+      3,4
+      3,0
+      8,4
+      1,10
+      2,14
+      8,10
+      9,0
+
+      fold along y=7
+      fold along x=5
+    ))
+  end
 
   describe '#part1_dots_count' do
     it 'counts the visible dots after completing the first fold' do
